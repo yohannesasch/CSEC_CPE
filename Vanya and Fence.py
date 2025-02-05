1ba46390149748e5d0687627931b9816d0ccd890
yohannesasch <yohannesaschalew3@gmail.com>
2025-02-05 10:23:38 +0300

    Create Vanya and Fence.py

---
+++ Vanya and Fence.py
@@ -0,0 +1,12 @@
+n, h = map(int, input().split())  
+heights = list(map(int, input().split()))  
+
+total_width = 0  
+
+ for height in heights:  
+    if height > h:  
+        total_width += 2 
+    else:  
+        total_width += 1  
+
+print(total_width)
